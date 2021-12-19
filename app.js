81a205da578eca032e5353e2d31f0d88d62c796b
dhalladay <dave.halladay@outlook.com>
2021-12-18 20:56:13 -0700

    add conditional about section question

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ const promptUser = () => {
           }
         }
       },
-      {type: 'input',
+      {
+      type: 'input',
       name: 'github',
       message: 'Enter your Github Username',
       validate: githubInput => {
@@ -39,9 +40,22 @@ const promptUser = () => {
       }
     },
     {
-      tyep: 'input',
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   ]);
 };
